640fada007c71203f35e89bfd6a5c5ceba327d78
Nathanael Wong <natgeo.wong@outlook.com>
2025-01-06 09:57:14 -0100

    Porting over the ETOPO-specific functions

--- src/ETOPO.jl
+++ src/ETOPO.jl
@@ -1,5 +1,27 @@
 module ETOPO
 
-# Write your package code here.
+using Dates
+using LandSea
+using Logging
+using Printf
+
+import LandSea: getLandSea
+
+using Reexport
+@reexport using RegionGrids
+@reexport using NCDatasets
+
+export
+        getLandSea
+
+struct ETOPODataset{ST<:AbstractString}
+    path :: ST
+end
+
+modulelog() = "$(now()) - ETOPO.jl"
+ETOPODataset(ST = String; path :: AbstractString = homedir()) = ETOPODataset{ST}(path)
+
+include("get.jl")
+include("backend.jl")
 
 end
